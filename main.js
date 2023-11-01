c88dc89d82b48274bc1676f6809be60fb3b5791f
FELIPE DASIL <125500685+Felipebyjus1@users.noreply.github.com>
2023-11-01 16:24:13 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,39 +1,29 @@
-noseX = 0
-noseY = 0
-fontSize = 0
-leftWristX = 0
-rightWristX = 0
+var song = ""
+var song2 = ""
 
 function setup(){
     canvas = createCanvas(600, 600)
     canvas.center()
-    background("black")
+    background("white")
     video = createCapture(VIDEO)
     video.size(600, 600)
+    video.hide()
 
     poseNet = ml5.poseNet(video, modelLoaded)
     poseNet.on("pose", gotPoses)
-} 
-
-function modelLoaded(){
-console.log("Modelo carregado!")
 }
-
-function gotPoses(results){
-    if(results.length > 0){
-      console.log(results)
-     noseX = results[0].pose.nose.x
-     noseY = results[0].pose.nose.y
-     leftWristX = results[0].pose.leftWrist.x
-     rightWristX = results[0].pose.rightWrist.x
-     fontSize = floor(leftWristX - rightWristX)
-     document.getElementById("resultado").innerHTML =  + fontSize
-    }
+function preload(){
+    song = loadSound("music.mp3")
+    song = loadSound("music2.mp3")
 }
-
 function draw(){
-    background("black")
-    textSize(25)
-    fill(white)
-    text("Estou sem criatividade então esse texto fica aqui para concluir o projeto :D", 200, 200)
+    image(video, 0, 0, 600, 600)
+    fill("black")
+    stroke("black")
+}
+function modelLoaded(){
+
+}
+function gotPoses(){
+
 }
